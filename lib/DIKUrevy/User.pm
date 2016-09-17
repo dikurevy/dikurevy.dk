05b38b83d716f937fa42c66c3d8a4bf5e98aff3e
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2016-09-17 18:36:15 +0200

    Allow funky unicode symbols in passwords

--- lib/DIKUrevy/User.pm
+++ lib/DIKUrevy/User.pm
@@ -3,6 +3,7 @@ use Mojo::Base 'DIKUrevy::DB';
 use utf8;
 
 use Crypt::PBKDF2;
+use Encode qw/encode/;
 use Mojo::Util qw/md5_sum secure_compare/;
 
 DIKUrevy::DB::db_fields(qw(
@@ -26,13 +27,15 @@ sub auth_user {
     my $user = $class->retrieve({ username => $username });
     return unless $user && $user->verified;
 
+    my $encoded_password = encode('UTF-8', $password);
+
     if ($user->password) {
         my $pbkdf2 = Crypt::PBKDF2->new();
-        return unless $pbkdf2->validate( $user->password, $password );
+        return unless $pbkdf2->validate( $user->password, $encoded_password );
     } elsif ($user->password_legacy) {
         # Legacy password authentication from Drupal
         # (Unsalted MD5; ugh.)
-        return unless secure_compare($user->password_legacy, md5_sum $password);
+        return unless secure_compare($user->password_legacy, md5_sum $encoded_password);
 
         $user->set_password( $password );
         $user->save();
@@ -45,8 +48,10 @@ sub set_password {
     my $self = shift;
     my $password = shift;
 
+    my $encoded_password = encode('UTF-8', $password);
+
     my $pbkdf2 = Crypt::PBKDF2->new();
-    $self->password( $pbkdf2->generate( $password ) );
+    $self->password( $pbkdf2->generate( $encoded_password ) );
     $self->password_legacy(undef);
     return $self;
 }
